5daf987eac8f9d627dda0842ba1f653072cbdd79
Jesus Sandoval <sandovalj9216@gmail.com>
2019-12-02 20:05:44 -0600

    added blurring for spheres

--- material.h
+++ material.h
@@ -56,7 +56,7 @@ class lambertian : public material {
         virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const  {
             // std::cout<<"LAMBERTIANSCATTER"<<std::endl;
              vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-             scattered = ray(rec.p, target-rec.p);
+             scattered = ray(rec.p, target-rec.p, r_in.time());
              attenuation = albedo;
              return true;
         }
--- raytracer.cpp
+++ raytracer.cpp
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 #include "sphere.h"
+#include "moving_sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
 #include "material.h"
@@ -52,14 +53,14 @@ hitable *random_scene() {
     hitable **list = new hitable*[n+1];
     list[0] =  new sphere(vec3(0,-1000,0), 1000, new lambertian(vec3(0.5, 0.5, 0.5)));
     int i = 1;
-    for (int a = -11; a < 11; a++) {
-        for (int b = -11; b < 11; b++) {
+    for (int a = -10; a < 10; a++) {
+        for (int b = -10; b < 10; b++) {
             float choose_mat = random_double();
             vec3 center(a+0.9*random_double(),0.2,b+0.9*random_double());
             if ((center-vec3(4,0.2,0)).length() > 0.9) {
                 if (choose_mat < 0.8) {  // diffuse
-                    list[i++] = new sphere(
-                        center, 0.2,
+                    list[i++] = new moving_sphere(
+                        center, center + vec3(0,0.5*random_double(), 0), 0.0, 1.0, 0.2,
                         new lambertian(vec3(random_double()*random_double(),
                                             random_double()*random_double(),
                                             random_double()*random_double()))
@@ -101,9 +102,9 @@ int main()
     vec3 lookfrom(13,2,3);
     vec3 lookat(0,0,0);
     float dist_to_focus = 10.0;
-    float aperture = 0.1;
+    float aperture = 0.0;
 
-    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus);
+    camera cam(lookfrom, lookat, vec3(0,1,0), 20, float(nx)/float(ny), aperture, dist_to_focus, 0.0, 1.0);
     for(int j=ny-1; j>=0; j--){
         for(int i=0; i<nx; i++){
             vec3 col(0,0,0);
